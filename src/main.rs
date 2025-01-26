3cc9e0dee3f684494b9a7c9dd2c3d6e7445478ce
Silcet <camorga1@gmail.com>
2025-01-26 18:31:07 +0100

    Fullstack with database

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,35 @@ use dioxus::prelude::*;
 
 static CSS: Asset = asset!("/assets/main.css");
 
+#[derive(serde::Deserialize)]
+struct DogApi {
+    message: String,
+}
+
 fn main() {
     dioxus::launch(App);
+}
+
+#[cfg(feature = "server")]
+thread_local! {
+    pub static DB: rusqlite::Connection = {
+        let conn = rusqlite::Connection::open("hotdog.db").expect("Failed to open the database");
+
+        conn.execute_batch(
+            "CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                url TEXT NOT NULL
+            );"
+        ).unwrap();
+
+        conn
+    }
+}
+
+#[server]
+async fn save_dog(image: String) -> Result<(), ServerFnError> {
+    DB.with(|f| f.execute("INSERT INTO dogs (url) VALUES (?1)", &[&image]))?;
+    Ok(())
 }
 
 #[component]
@@ -26,16 +53,30 @@ fn Title() -> Element {
 
 #[component]
 fn DogView() -> Element {
-    let img_src = use_hook(|| "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg");
-    let skip = move |evt| {};
-    let save = move |evt| {};
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap()
+            .message
+    });
+
+    let save = move |_| async move {
+        let current = img_src.cloned().unwrap();
+
+        img_src.restart();
+
+        save_dog(current).await.unwrap();
+    };
 
     rsx! {
         div { id: "dogview",
-            img { src: "{img_src}" }
+            img { src: img_src.cloned().unwrap_or_default() }
         }
         div { id: "buttons",
-            button { id: "skip", onclick: skip, "skip" }
+            button { id: "skip", onclick: move |_| img_src.restart(), "skip" }
             button { id: "save", onclick: save, "save!" }
         }
     }
